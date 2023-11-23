57aecd21682168bfa8631ec96924b222dbe93cc3
Davide Luppoli <davide@davideluppoli.it>
2023-11-23 12:25:17 +0100

    ASP NET Crud

--- API/Controllers/SportsController.cs
+++ API/Controllers/SportsController.cs
@@ -45,5 +45,70 @@ namespace API.Controllers
                 return View(s);
             }
         }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            ViewBag.Title = "Modifica sport";
+
+            using (var context = new OlympicsContext())
+            {
+                var s = await context.Events.FirstOrDefaultAsync(w => w.Id == id);
+                return View(s);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Event e)
+        {
+            using (var context = new OlympicsContext())
+            {
+                var candidate = await context.Events.FirstOrDefaultAsync(w => w.Id == e.Id);
+                candidate.Sport = e.Sport;
+                candidate.Event1 = e.Event1;
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Event e)
+        { 
+            using(var context = new OlympicsContext())
+            {
+                context.Events.Add(e);
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            ViewBag.Title = "Cancellazione sport";
+
+            using (var context = new OlympicsContext())
+            {
+                var s = await context.Events.FirstOrDefaultAsync(w => w.Id == id);
+                return View(s);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(Event e)
+        {
+            using (var context = new OlympicsContext())
+            {
+                var candidate = await context.Events.FirstOrDefaultAsync(w => w.Id == e.Id);
+                context.Events.Remove(candidate);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
